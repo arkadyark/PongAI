56e73b2a482085bf6c61504296f587fe6e2a3c15
Arkady Arkhangorodsky <arkady.arkhangorodsky@mail.utoronto.ca>
2014-10-21 19:41:38 -0400

    Made the template for aggressively attacking opponent - now all that's needed is to implement the two functions

--- straight_no_chaser_ai.py
+++ straight_no_chaser_ai.py
@@ -12,18 +12,18 @@ def move_getter(paddle_frect, other_paddle_frect, ball_frect, table_size):
 
     Arguments:
     paddle_frect: a rectangle representing the coordinates of the paddle
-                  paddle_frect.pos[0], paddle_frect.pos[1] is the top-left
-                  corner of the rectangle. 
-                  paddle_frect.size[0], paddle_frect.size[1] are the dimensions
-                  of the paddle along the x and y axis, respectively
+		  paddle_frect.pos[0], paddle_frect.pos[1] is the top-left
+		  corner of the rectangle. 
+		  paddle_frect.size[0], paddle_frect.size[1] are the dimensions
+		  of the paddle along the x and y axis, respectively
 
     other_paddle_frect:
-                  a rectangle representing the opponent paddle. It is formatted
-                  in the same way as paddle_frect
+		  a rectangle representing the opponent paddle. It is formatted
+		  in the same way as paddle_frect
     ball_frect:   a rectangle representing the ball. It is formatted in the 
-                  same way as paddle_frect
+		  same way as paddle_frect
     table_size:   table_size[0], table_size[1] are the dimensions of the table,
-                  along the x and the y axis respectively
+		  along the x and the y axis respectively
 
     The coordinates look as follows:
 0             x
@@ -36,18 +36,11 @@ def move_getter(paddle_frect, other_paddle_frect, ball_frect, table_size):
     global turn_number
 
     if turn_number == 1:
-<<<<<<< HEAD
 	initialize(paddle_frect, other_paddle_frect, table_size[0])
     turn_number += 1
     if move_getter.previous_ball_pos != None:
 	move_getter.ball_vel = get_vel(move_getter.previous_ball_pos, ball_frect.pos)
 	move_getter.opponent_vel = other_paddle_frect.pos[1] - move_getter.previous_opponent_pos
-=======
-        initialize(paddle_frect, table_size[0])
-    turn_number += 1
-    if move_getter.previous_ball_pos != None:
-        move_getter.ball_vel = get_vel(move_getter.previous_ball_pos, ball_frect.pos)
->>>>>>> c4d45b6f6489b820236a99ff2fade393251c9def
     move_getter.previous_ball_pos = ball_frect.pos
     move_getter.previous_opponent_pos = other_paddle_frect.pos[1]
     if len(move_getter.previous_opponent_vel) >= opponent_vel_history:
@@ -57,27 +50,33 @@ def move_getter(paddle_frect, other_paddle_frect, ball_frect, table_size):
     moving_left = move_getter.ball_vel[0] < 0
     moving_away = moving_left != move_getter.left_paddle
     if moving_left:
-<<<<<<< HEAD
 	paddle_edge = paddle_frect.size[0]
 	other_paddle_edge = paddle_frect.pos[0]
     else:
 	paddle_edge = paddle_frect.pos[0]
 	other_paddle_edge = paddle_frect.size[0]
     if not moving_away:
+	# Ball is coming to me
 	trajectory = get_ball_trajectory(move_getter.ball_vel, ball_frect.pos, paddle_edge, table_size[1])
 	estimated_ball_pos = trajectory['position'] + ball_frect.size[1]/2
-	if out_of_reach(paddle_frect, trajectory):
-	    # TODO - dangerous with walls
-	    print "I can't reach the ball, moving to centre"
+	possible_collision_positions = get_possible_positions(trajectory, paddle_frect)
+	paddle_target_position = get_optimal_target(possible_collision_positions, paddle_frect, other_paddle_frect, move_getter.ball_vel)
+	if out_of_reach(paddle_frect, trajectory, True):
+	    # Out of reach, don't bother
+	    if not turn_number % 10:
+		print "I can't reach the ball, moving to centre"
 	    move_getter.target_y = table_size[1]*0.5
 	else:
-	    print "The ball is actually coming to:", estimated_ball_pos
+	    if not turn_number % 20:
+		print "The ball is actually coming to:", estimated_ball_pos
 	    move_getter.target_y = estimated_ball_pos
     else:
+	# Ball is going at opponent
 	trajectory = get_ball_trajectory(move_getter.ball_vel, ball_frect.pos, paddle_edge, table_size[1], 0)
-	if out_of_reach(other_paddle_frect, trajectory):
+	if out_of_reach(other_paddle_frect, trajectory, False):
 	    # Move to middle if point is over
-	    print "The opponent can't reach the ball, moving to centre"
+	    if not turn_number % 10:
+		print "The opponent can't reach the ball, moving to centre"
 	    move_getter.target_y = table_size[1]/2
 	else:
 	    # Assuming opponent continues their trajectory
@@ -100,29 +99,24 @@ def move_getter(paddle_frect, other_paddle_frect, ball_frect, table_size):
 		projected_vel[0] = 0.95*(2*(not move_getter.left_paddle - 1))
 	    projected_vel = (projected_vel[0]*PADDLE_BOUNCE, projected_vel[1]*PADDLE_BOUNCE)
 	    return_trajectory = get_ball_trajectory(projected_vel, (paddle_edge, trajectory['position']), other_paddle_edge, table_size[1])
-	    print "I think the opponent will hit it to:", return_trajectory['position'] 
+	    if not turn_number % 20:
+		print "I think the opponent will hit it to:", return_trajectory['position'] 
+	    paddle_target_position = paddle_frect.pos[1] + paddle_frect.size[1]/2
 	    move_getter.target_y = return_trajectory['position'] 
-=======
-        paddle_edge = paddle_frect.size[0]
+    if paddle_target_position < move_getter.target_y:
+	return "down"
     else:
-        paddle_edge = paddle_frect.pos[0]
-    if not moving_away:
-         move_getter.target_y = get_ball_trajectory(move_getter.ball_vel, ball_frect.pos, paddle_edge, table_size[1], 0)[0] + ball_frect.size[1]/2
-    else:
-        move_getter.target_y = table_size[1]/2
->>>>>>> c4d45b6f6489b820236a99ff2fade393251c9def
-    if paddle_frect.pos[1]+paddle_frect.size[1]/2 < move_getter.target_y:
-        return "down"
-    else:
-        return "up"
+	return "up"
 
-def out_of_reach(paddle_frect, trajectory):
+def out_of_reach(paddle_frect, trajectory, me):
     distance_to_ball = abs(paddle_frect.pos[1] + paddle_frect.size[1]*0.5 - trajectory['position'])
-    return distance_to_ball - trajectory['time'] > paddle_frect.size[1]*0.5 + 25*trajectory['walls']
+    if me:
+	return distance_to_ball - trajectory['time'] > paddle_frect.size[1]*0.5 + 45*trajectory['walls']
+    else:
+	return distance_to_ball - trajectory['time'] > paddle_frect.size[1]*0.5 + 20*trajectory['walls']
 
 def get_vel(previous_pos, current_pos):
     return (current_pos[0] - previous_pos[0],\
-<<<<<<< HEAD
 	    current_pos[1] - previous_pos[1])
 
 def get_angle(paddle_y, paddle_height, ball_y, left_paddle):
@@ -131,9 +125,6 @@ def get_angle(paddle_y, paddle_height, ball_y, left_paddle):
     rel_dist_from_c = min(0.5, rel_dist_from_c)
     rel_dist_from_c = max(-0.5, rel_dist_from_c)
     sign = 1-2*left_paddle
-=======
-            current_pos[1] - move_getter.previous_ball_pos[1])
->>>>>>> c4d45b6f6489b820236a99ff2fade393251c9def
 
     return sign*rel_dist_from_c*MAX_ANGLE*math.pi/180
 
@@ -147,7 +138,6 @@ def get_ball_trajectory(ball_vel, ball_pos, paddle_edge, table_height, time = 0,
     time_to_bottom = (table_height - ball_pos[1])/ball_vel[1]
     time_to_wall = max(time_to_top, time_to_bottom)
     if (time_to_wall < time_to_edge):
-<<<<<<< HEAD
 	# Headed for a wall
 	projected_ball_vel = (ball_vel[0], -ball_vel[1])
 	if time_to_top > time_to_bottom:
@@ -160,18 +150,14 @@ def get_ball_trajectory(ball_vel, ball_pos, paddle_edge, table_height, time = 0,
 	# Headed for a paddle (or edge)
 	projected_y = ball_pos[1] + time_to_edge*ball_vel[1]
 	return {'position':projected_y, 'time':time_to_edge + time, 'walls':walls}
-=======
-        projected_ball_vel = (ball_vel[0], -ball_vel[1])
-        if time_to_top > time_to_bottom:
-            projected_ball_pos = (ball_pos[0] + time_to_top*ball_vel[0], 0)
-        else:
-            projected_ball_pos = (ball_pos[0] + time_to_bottom*ball_vel[0], table_height)
-        return get_ball_trajectory(projected_ball_vel, projected_ball_pos,\
-                    paddle_edge, table_height, time + time_to_wall) 
-    else:
-        projected_y = ball_pos[1] + time_to_edge*ball_vel[1]
-        return projected_y, time_to_edge + time
->>>>>>> c4d45b6f6489b820236a99ff2fade393251c9def
+
+def get_possible_positions(trajectory, paddle_frect):
+    # TODO - implement
+    return []
+
+def get_optimal_target(possible_collision_positions, paddle_frect, other_paddle_frect, ball_vel):
+    # TODO - implement
+    return paddle_frect.pos[1] + paddle_frect.size[1]/2
 
 def initialize(paddle_frect, their_paddle_frect, table_width):
     move_getter.left_paddle = paddle_frect.pos[0] < table_width/2
